73e8501a105aa13fd31315bbad6b87d84862aaa3
Gareth Robins <gareth.robins@gmail.com>
2016-05-25 07:46:36 +1200

    update R

--- csv_to_geojson.R
+++ csv_to_geojson.R
@@ -12,6 +12,7 @@ schools <- data.table::fread('c:/sources/schoolmap/data/directory-school-current
 schools.min <- schools[, c('Name', 'Longitude', 'Latitude', 'Decile', 'Total School Roll', 'European/ Pakeha', 'Maori', 'Pasifika', 'Asian', 'MELAA', 'Other', 'International Students'), with=F]
 schools.min <- as.data.frame(schools.min)
 schools.min <- schools.min %>% filter(!is.na(Longitude) | !is.na(Latitude))
+schools.min <- schools.min %>% filter(Longitude > 170)
 schools.sp <- SpatialPointsDataFrame(schools.min[,c(2, 3)],schools.min[,-c(2, 3)])
 str(schools.sp) # Now is class SpatialPointsDataFrame
 
