2a0823ff97b16489a86d3c58064649e0977ea0fc
Gareth Robins <gareth.robins@gmail.com>
2016-05-24 21:04:59 +1200

    first commit

---
+++ csv_to_geojson.R
@@ -0,0 +1,19 @@
+#Write geojson
+#====
+
+#Load libraries
+library(rgdal)
+require(dplyr)
+
+#dataMap is a dataframe with coordinates on cols 11 (LATITUDE) and 12 (LONGITUDE)
+#Transfor coordinates to numeric
+
+schools <- data.table::fread('c:/sources/schoolmap/data/directory-school-current.csv', header=T, sep=',')
+schools.min <- schools[, c('Name', 'Longitude', 'Latitude', 'Decile', 'Total School Roll', 'European/ Pakeha', 'Maori', 'Pasifika', 'Asian', 'MELAA', 'Other', 'International Students'), with=F]
+schools.min <- as.data.frame(schools.min)
+schools.min <- schools.min %>% filter(!is.na(Longitude) | !is.na(Latitude))
+schools.sp <- SpatialPointsDataFrame(schools.min[,c(3, 2)],schools.min[,-c(3, 2)])
+str(schools.sp) # Now is class SpatialPointsDataFrame
+
+#Write as geojson
+writeOGR(schools.sp, 'c:/sources/schoolmap/data/schools.geojson','dataMap', driver='GeoJSON')
